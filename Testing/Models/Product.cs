1e8458ed97dfcab5399a51c4cacbc476ab482fb8
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-06-10 15:21:01 -0500

    messed something up

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -15,7 +15,7 @@ namespace Testing.Models
         public double Price { get; set; }
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
-        public int StockLevel { get; set; } //might be string
+        public int StockLevel { get; set; }
         public IEnumerable<Category> Categories { get; set; }
     }
 }
